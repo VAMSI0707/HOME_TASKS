994a46178cfe05f2eca3e9a8dcff0fc6efd95634
Vamsi Kalisetty <hello@kvs.com>
2023-02-09 14:15:29 +0530

    Implemted hometask given on javascript second session

---
+++ hometask2.js
@@ -0,0 +1,130 @@
+/*
+Get the Middle Character
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+*/
+function getMiddle(s) {
+    const stringLength = s.length;
+    const middle = parseInt(stringLength / 2);
+    if (stringLength % 2 != 0) {
+        return s[middle];
+    }
+    return s[middle - 1] + s[middle];
+}
+
+/*
+Partition On
+DESCRIPTION:
+Write a function which partitions a list of items based on a given predicate.
+
+After the partition function is run, the list should be of the form [ F, F, F, T, T, T ] where the Fs (resp. Ts) are items for which the predicate function returned false (resp. true).
+
+NOTE: the partitioning should be stable; in other words: the ordering of the Fs (resp. Ts) should be preserved relative to each other.
+
+For convenience and utility, the partition function should return the boundary index. In other words: the index of the first T value in items.
+
+For example:
+var items = [1, 2, 3, 4, 5, 6];
+function isEven(n) {return n % 2 == 0}
+var i = partitionOn(isEven, items);
+// items should now be [1, 3, 5, 2, 4, 6]
+// i     should now be 3
+
+*/
+function partitionOn(pred, items) {
+    let truePredicate = items.filter(item => pred(item));
+    let falsePredicate = items.filter(item => !pred(item));
+    let i = 0;
+    for (let item of falsePredicate) {
+        items[i++] = item;
+    }
+    for (let item of truePredicate) {
+        items[i++] = item;
+    }
+    return falsePredicate.length;
+}
+
+/*
+Calculating with Functions
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+seven(times(five())); // must return 35
+four(plus(nine())); // must return 13
+eight(minus(three())); // must return 5
+six(dividedBy(two())); // must return 3
+Requirements:
+
+There must be a function for each number from 0 ("zero") to 9 ("nine")
+There must be a function for each of the following mathematical fs: plus, minus, times, dividedBy
+Each calculation consist of exactly one f and two numbers
+The most outer function represents the left operand, the most inner function represents the right operand
+Division should be integer division. For example, this should return 2, not 2.666666...:
+eight(dividedBy(three()));
+*/
+
+function zero(f) {
+    return f ? f(0) : 0;
+}
+function one(f) {
+    return f ? f(1) : 1;
+}
+function two(f) {
+    return f ? f(2) : 2;
+}
+function three(f) {
+    return f ? f(3) : 3;
+}
+function four(f) {
+    return f ? f(4) : 4;
+}
+function five(f) {
+    return f ? f(5) : 5;
+}
+function six(f) {
+    return f ? f(6) : 6;
+}
+function seven(f) {
+    return f ? f(7) : 7;
+}
+function eight(f) {
+    return f ? f(8) : 8;
+}
+function nine(f) {
+    return f ? f(9) : 9;
+}
+function plus(b) {
+    return function (a) {
+        return a + b;
+    };
+}
+function minus(b) {
+    return function (a) {
+        return a - b;
+    };
+}
+function times(b) {
+    return function (a) {
+        return a * b;
+    };
+}
+function dividedBy(b) {
+    return function (a) {
+        return Math.floor(a / b);
+    };
+}
