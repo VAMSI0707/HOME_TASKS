f6294b3095d30ee33f6426b35b6644be135da6ab
VAMSI SASHANK <80934948+VAMSI0707@users.noreply.github.com>
2023-05-15 11:43:34 +0530

    Update app.component.spec.ts

--- angular_tasks/angular_task1/src/app/app.component.spec.ts
+++ angular_tasks/angular_task1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular_hometask_1'`, () => {
+  it(`should have as title 'angular-task1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular_hometask_1');
+    expect(app.title).toEqual('angular-task1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular_hometask_1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-task1 app is running!');
   });
 });
